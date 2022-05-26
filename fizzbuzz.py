8edc85f41ed5593c6f78b321bf4768fca5db4480
mrugayam <70637787+mrugayam@users.noreply.github.com>
2022-05-26 10:52:29 +0100

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,28 +1,19 @@
-from comet_ml import Experiment 
 from codecarbon import EmissionsTracker
 
 tracker = EmissionsTracker()
 tracker.start()
 
-def fizz_bizz():
- 
-    for num in range(1, 20):
-        if num % 15 == 0:
-            print("FizzBuzz")
-        elif num % 3 == 0:
-            print("Fizz") 
-        elif num % 5 == 0:
-            print("Buzz")
+
+for num in range(1, 1000):
+    if num % 15 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz") 
+    elif num % 5 == 0:
+        print("Buzz")
     else:
         print(num) 
 
 tracker.stop()
 emissions: float = tracker.stop()
-#log_metric()
 print(emissions)
-        
-           
-    
-
-
-
